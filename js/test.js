0cbbeab63e526f614bf94a7160bcfd400a555c13
Austin Dobrusky <20015@shs.local>
2018-03-27 14:10:59 -0500

    Changed some random stuff.

--- js/test.js
+++ js/test.js
@@ -3,5 +3,5 @@ var navItemWidth = 80;
 var carouselInterval = 10000;
 var carouselFade = 200;
 var navDuration = 600;
-var navShrink = 2.5;
+var navShrink = 1.7;
 var carouselDirection = 'right'; //Accepts 'right' or 'left'
